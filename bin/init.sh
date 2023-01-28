0e24c223dd0951d61f5adfa033da644765729a70
Luis Ignacio Ayala Gonzalez <luisayala@MacBook-Pro-de-Luis.local>
2023-01-28 03:23:28 -0600

    feat: change variables source

--- bin/init.sh
+++ bin/init.sh
@@ -1,3 +1,8 @@
+source .env.sample
+source ~/.zshrc
+
+ORGANIZATION=luis-ayala15
+
 echo "Authorizing GitHub"
 gh auth login --hostname github.ibm.com
 echo "Authorized GitHub"
@@ -6,13 +11,13 @@ template=$(git remote get-url --push origin)
 echo "Enter the name of the repository: "
 read nameOfRepo
 
-gh repo create luis-ayala15/${nameOfRepo} --template="${template}" --public
+gh repo create ${ORGANIZATION}/${nameOfRepo} --template="${template}" --public
 echo "Succesfully initialized ${nameOfRepo}"
 cd ../
 echo "root folder"
 echo "Cloning Repo ${nameOfRepo}"
 sleep 10
-gh repo clone luis-ayala15/${nameOfRepo} ${nameOfRepo}
+gh repo clone ${ORGANIZATION}/${nameOfRepo} ${nameOfRepo}
 returnCode=$?
 if [ "${returnCode}" != "0" ]; then
     echo "GH failed"
@@ -24,13 +29,13 @@ tmp=$(mktemp)
 
 repo=$(git remote get-url --push origin)
 
-sed -i '' "/sonar.projectKey=/ s/=.*/=${nameOfRepo}/" sonar-project.properties
+sed -i '' "/sonar.projectKey=/ s/=.*/=${ORGANIZATION}/" sonar-project.properties
 returnCode=$?
 if [ "${returnCode}" != "0" ]; then
     echo "Sed failed"
     exit 1
 fi
-sed -i '' "/sonar.projectName=/ s/=.*/=${nameOfRepo}/" sonar-project.properties
+sed -i '' "/sonar.projectName=/ s/=.*/=${ORGANIZATION}/" sonar-project.properties
 returnCode=$?
 if [ "${returnCode}" != "0" ]; then
     echo "Sed failed"
@@ -42,8 +47,8 @@ jq '.name = "'${nameOfRepo}'"' package.json > "$tmp" && mv "$tmp" package.json
 jq '.description = "'${nameOfRepo}'"' package.json > "$tmp" && mv "$tmp" package.json
 echo "Package.json file updated name and description with value ${nameOfRepo}"
 jq '.repository.url = "git+'${repo}'"' package.json > "$tmp" && mv "$tmp" package.json
-jq '.bugs.url = "https://github.com/luis-ayala15/'${nameOfRepo}'/issues"' package.json > "$tmp" && mv "$tmp" package.json
-jq '.homepage = "https://github.com/luis-ayala15/'${nameOfRepo}'#readme"' package.json > "$tmp" && mv "$tmp" package.json
+jq '.bugs.url = "https://github.com/'${ORGANIZATION}'/'${nameOfRepo}'/issues"' package.json > "$tmp" && mv "$tmp" package.json
+jq '.homepage = "https://github.com/'${ORGANIZATION}'/'${nameOfRepo}'#readme"' package.json > "$tmp" && mv "$tmp" package.json
 echo "Package.json file updated relevant urls with value ${nameOfRepo}"
 
 #nvm use
@@ -53,6 +58,7 @@ git commit -m "feat: update project config files"
 git push
 npx snyk monitor
 npm run sonarcloud
+
 echo "Repo URL: ${repo}"
 echo "Opening project in Visual Studio"
 code .
