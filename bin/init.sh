34b6e9c669e89506156397123e01760d3612ded4
Luis Ignacio Ayala Gonzalez <luisayala@MacBook-Pro-de-Luis.local>
2023-01-29 01:34:38 -0600

    feat: change name of repo

--- bin/init.sh
+++ bin/init.sh
@@ -36,7 +36,7 @@ if [ "${returnCode}" != "0" ]; then
     echo "Sed failed"
     exit 1
 fi
-sed -i '' "/sonar.projectName=/ s/=.*/=${ORGANIZATION}/" sonar-project.properties
+sed -i '' "/sonar.projectName=/ s/=.*/=${nameOfRepo}/" sonar-project.properties
 echo "SONAR_TOKEN="$SONAR_TOKEN >> .env
 
 returnCode=$?
